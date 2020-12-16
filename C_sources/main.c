43f0a52ec7f67b4929d5fb380a270d8aa99633e7
Ekaterina Pridius <61543837+pringleskate@users.noreply.github.com>
2020-12-16 10:23:08 +0300

    Рефакторинг

--- C_sources/main.c
+++ C_sources/main.c
@@ -7,8 +7,8 @@ __attribute__((section(".entry")))
 int main(int argc, char *argv[])
 {
 	catch_enter();
-    get_CPUID(0);
-    get_CPUID(1);
+    	get_CPUID(0);
+    	get_CPUID(1);
 	get_MSR(EFER_REG);
 
   	return 0;
